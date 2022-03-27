4cee89bda532abd0c2b19be59fc1bd57d0be4316
Sandra Malinova <90518039+MalininiGrande@users.noreply.github.com>
2022-03-27 10:42:45 +0200

    cyklus while ukol

--- main.js
+++ main.js
@@ -3,11 +3,22 @@
 
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
+// console.log('První příklad - vzestupná řada 0 - 10');
+// let vzestupnaCisla = [];
 
-//TADY NAPIS CYKLUS
 
-console.log('-------------------');
+// for (let i = 0; i <= 10; i += + 1) {
+//     console.log(i);
+// }
+
+
+console.log('První příklad - vzestupná řada 0 - 10 - cyklus while');
+let a = 0;
+while(a<=10) {
+    console.log(a);
+    a++;  
+}
+
 
 
 
@@ -15,17 +26,30 @@ console.log('-------------------');
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
-console.log('Druhý příklad - rozdíl mezi <= a <');
+console.log('Druhý příklad - rozdíl mezi <= a < - cyklus while');
 
-//TADY NAPIS CYKLUS
+// for (let i = 0; i < 11; i += + 1) {
+//     console.log(i);
+// }
 
-console.log('-------------------');
+let b = 0;
+while(b<10) {
+    console.log(b);
+    b++;  
+}
+
 
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
+console.log('Třetí příklad - sestupná řada 10 - 0 - cyklus while');
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
+// //TADY NAPIS CYKLUS
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
+let c = 10;
+while(c>=0) {
+    console.log(c);
+  c--;
+}
